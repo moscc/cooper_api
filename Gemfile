30d35c0f27d547ee60e1195ef8d16538eaa44101
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-05-18 09:30:05 +0200

    reworked routes

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby '2.3.7'
+ruby '2.4.1'
 
 gem 'devise_token_auth'
 gem 'rails', '~> 5.1.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-
-    namespace :v1, defaults: {format: :json } do
+    resources :pings, only: [:index], constraints: { format: 'json' }
+  end
+    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create, :index]
     end
