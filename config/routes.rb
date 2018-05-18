787768dca1b794f00f069da234947d0dd06dd89b
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-05-18 09:45:03 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    resources :pings, only: [:index], constraints: { format: 'json' }
-  end
+    namespace :v0 do
+      resources :pings, only: [:index], constraints: { format: 'json' }
+    end
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create, :index]
